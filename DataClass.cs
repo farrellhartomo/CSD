f261a56ca26c4da496afefd7ea3edc2036e9abbf
farrellhartomo <farrell.yodihartomo@gmail.com>
2019-03-29 08:59:38 +0100

    edit data

---
+++ DataClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSD
+{
+    public class person
+    {
+        public string personID { get; set; }
+        public string personFirstName { get; set; }
+        public string personLastName { get; set; }
+
+        public person(string firstname,string lastname)
+        {
+            this.personFirstName = firstname;
+            this.personFirstName = lastname;
+        }
+
+        public person(string ID, string firstname, string lastname)
+        {
+            this.personID = ID;
+            this.personFirstName = firstname;
+            this.personFirstName = lastname;
+        }
+    }
+
+    public class address
+    {
+        public address(string street, string city, string state, string postNumber)
+        {
+            this.street = street;
+            this.city = city;
+            this.state = state;
+            this.postNumber = postNumber;
+        }
+
+        public string street { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string postNumber { get; set; }
+        
+    }
+
+    public class personalData
+    {
+        List<person> person = new person();
+    }
+
+    interface IMapping
+    {
+
+    }
+}
